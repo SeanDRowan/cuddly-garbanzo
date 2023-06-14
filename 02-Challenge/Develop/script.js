21c4d6fde420b976516ed8572d0de9b554f8653e
Sean <sdrowan93@gmail.com>
2023-06-13 20:28:02 -0500

    added for loop

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,15 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+  var letters= [ 'A','B','C']
+  var numbers= [ 1,2,3,4,5,6,7,8,9]
+  var spec = ['!','@','#','$','%','&','*']
+
 function generatePassword(){
- var letters = [ "A","B","C"] 
- var numbers= [ 1,2,3,4,5,6,7,8,9]
- var spec = ["!","@","#","$","%","&","*"]
-for (x in letters){
-  
+
+  var passL =prompt("choose password length")
+// lists random numbers from the numbers array until if reaches passL
+for (var i= 0; i < passL; i++){ 
+  var index = Math.floor(Math.random() * passL);
+  var RN = numbers[index];
+  console.log( numbers[RN])
+  }
+  var characters = prompt( "characters availbalbe")
 }
 
-}
+generatePassword();
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
