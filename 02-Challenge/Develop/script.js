183f423022dc51ee002ec3379fea2f82e7dde628
Sean <sdrowan93@gmail.com>
2023-06-16 15:02:27 -0500

    rework, changed arrays to stings and added .charAt

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,65 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-const options ={
-letters: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-lettersLc: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-numbers: [ 1,2,3,4,5,6,7,8,9],
-specl: ['!','@','#','$','%','&','*'],
-}
-
-var pwOptions = {
-  length: passL,
-  hasSpecial: specC,
-  hasNumbers: num,
-  hasCapital: capL,
-}
+let letters ="abcdefghijklmnopqustuvwxyz";
+let numbers ="12345abcdefghijklmnopqustuvwxyz67890";
+let spec = "!@#$abcdefghijklmnopqustuvwxyz%^&*";
+let all = "!@#$1234567890abcdefghijklmnopqustuvwxyz$%^&*"
 
   var passL =prompt("choose password length")
   alert("your password will be "+ passL + " characters long")
 
-  var capL =prompt ("Include Capital letters? Y/N").toUpperCase(); 
+  var capL =prompt ("Capital letters? Y/N").toUpperCase(); 
  
   var num= prompt ("Include numbers? Y/N").toUpperCase(); 
 
   var specC = prompt ("include special characters? Y/N").toUpperCase(); 
 
-
+console.log(capL,num,specC)
 function generatePassword(){
-  let LC = "";
-  for (lettersLc in options) {
-    LC += options.lettersLc [Math.floor(Math.random() * passL)];
-    console.log(LC)
-  }
-  let UC = "";
-  for (letters in options){ UC += options.letters [Math.floor(Math.random() * passL)];
-    console.log(UC)
-  }
-  let num = "";
-  for (numbers in options){ num += options.numbers [Math.floor(Math.random() * passL)];
-    console.log(num)
-  }
-  let SC ="";
-  for (specl in options){ SC += options.specl [Math.floor(Math.random() * passL)];
-    console.log(SC)
-  }
-  if (capL == "Y" && num =="N" && specC== "N")
-  {
-  for (numbers in options ) LC += options.letters [Math.floor(Math.random() * passL)];
-    console.log(UC)
-   var password1 = [LC].concat ([UC])
+  let password1 = "";
+  if (capL=== "N" && num === "N" && specC === "N"){
+  for (var i =0; i < passL; i++) {
+    password1  = password1 + letters.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
+  } alert("Your password will contain lowercase letters only")
+} else if (capL === "Y" && num ==="N" && specC=== "N"){for (var i =0; i < passL; i++) {
+  password1 = password1 + letters.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
+} alert("Your password will contain capital letters")
+} else if (capL === "N" && num ==="Y" && specC=== "N"){for (var i =0; i < passL; i++) {
+  password1 = password1 + numbers.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
+}alert("Your password will contain numbers")
+} else if (capL === "N" && num ==="N" && specC=== "Y"){for (var i =0; i < passL; i++) {
+  password1 = password1 + spec.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
+}alert("Your password will contain special characters")
+}else if (capL === "Y" && num ==="Y" && specC=== "N"){for (var i =0; i < passL; i++) {
+  password1 = password1 + numbers.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
+}alert("Your password will contain capital letters and numbers")
+}else if (capL === "Y" && num ==="N" && specC=== "Y"){for (var i =0; i < passL; i++) {
+  password1 = password1 + spec.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
+}alert("Your password will contain capital letters and special characters")
+}else if (capL === "N" && num ==="Y" && specC=== "Y"){for (var i =0; i < passL; i++) {
+  password1 = password1 + all.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
+}alert("Your password will contain numbers and special characters")
+} else if (capL === "Y" && num ==="Y" && specC=== "Y"){for (var i =0; i < passL; i++) {
+  password1 = password1 + all.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
+}alert("Your password will contain all available characters")
+}else{
+  alert( "invalid input") 
+}
   console.log(password1)
-  }else{
-    for (numbers in options ) LC += options.letters [Math.floor(Math.random() * (0.5* passL))];
-   console.log(password1)
-  }
     }
 generatePassword();
-// if capital letters only - RCL
-//if lowercase letter
-// if numbers - RN
-// if spec characters -SC
-//lists random numbers from the numbers array until if reaches passL
+
+
 
 //Write password to the #password input
 function writePassword() {
