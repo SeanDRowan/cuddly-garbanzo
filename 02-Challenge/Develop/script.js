dbcac0315af8063fe097c37aa5057e0f6e156730
Sean <sdrowan93@gmail.com>
2023-06-19 16:39:42 -0500

    improvements after tutoring session, changed prompts to confirm, put character options into arrays, removed redundencies.

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,61 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //setting up variables
-let letters ="abcdefghijklmnopqustuvwxyz";
-let numbers ="12345abcdefghijklmnopqustuvwxyz67890";
-let spec = "!@#$abcdefghijklmnopqustuvwxyz%^&*";
-let all = "!@#$1234567890abcdefghijklmnopqustuvwxyz$%^&*"
-let password1 = "";
+let capLetters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let lowercaseL = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let numbers = ["1","2","3","4","5","7","8","9","0"]
+let spec = ["!","@","#","$","%","^","&","*"];
+
+
 
 //Generate password function
 function generatePassword(){
-  //Conditionals for password length/Caps/numbers/ and special character promts
-  var passL =prompt("How long will your password be? Choose a number between 8-123")
-  if ( 8> passL){
+ 
+  let password1 = [];
+  var characterBank = [];
+ // ends function if user choose a length too long or too short
+  var passL =prompt("How long will your password be? Choose a number between 8-128")
+  if ( passL < 8 || passL > 128){
   alert ( "please choose a number between 8-128")
-    generatePassword()
-  }else if ( passL > 128) {
-  alert ( "please choose a number between 8-128")
-   generatePassword()
   }
-  else{alert("your password will be "+ passL + " characters long")
-  var capL =prompt ("Capital letters? Y/N").toUpperCase(); 
-  var num= prompt ("Include numbers? Y/N").toUpperCase(); 
-  var specC = prompt ("include special characters? Y/N").toUpperCase();} 
-// conditionals for user choice to prompts + "for" loops to pull random characcters from selected variable strings
-  if (capL=== "N" && num === "N" && specC === "N"){
-  for (var i =0; i < passL; i++) {
-    password1  = password1 + letters.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
-  } alert("Your password will contain lowercase letters only")
-} else if (capL === "Y" && num ==="N" && specC=== "N"){for (var i =0; i < passL; i++) {
-  password1 = password1 + letters.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
-} alert("Your password will contain capital letters")
-} else if (capL === "N" && num ==="Y" && specC=== "N"){for (var i =0; i < passL; i++) {
-  password1 = password1 + numbers.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
-}alert("Your password will contain numbers")
-} else if (capL === "N" && num ==="N" && specC=== "Y"){for (var i =0; i < passL; i++) {
-  password1 = password1 + spec.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
-}alert("Your password will contain special characters")
-}else if (capL === "Y" && num ==="Y" && specC=== "N"){for (var i =0; i < passL; i++) {
-  password1 = password1 + numbers.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
-}alert("Your password will contain capital letters and numbers")
-}else if (capL === "Y" && num ==="N" && specC=== "Y"){for (var i =0; i < passL; i++) {
-  password1 = password1 + spec.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
-}alert("Your password will contain capital letters and special characters")
-}else if (capL === "N" && num ==="Y" && specC=== "Y"){for (var i =0; i < passL; i++) {
-  password1 = password1 + all.charAt(Math.floor(Math.random() * Math.floor(letters.length -1)));
-}alert("Your password will contain numbers and special characters")
-} else if (capL === "Y" && num ==="Y" && specC=== "Y"){for (var i =0; i < passL; i++) {
-  password1 = password1 + all.charAt(Math.floor(Math.random() * Math.floor(letters.length -1))).toUpperCase();
-}alert("Your password will contain all available characters")
-}else{
-  alert( "invalid input") 
-} 
-} 
+// confirm boxes for the different variables
+  var capL = confirm("Capital letters?")
+  var lowerL = confirm("Lowercase letters?")
+  var numbersC =confirm("Numbers?")
+  var specC = confirm("Special Characters?")
+// considtionals to verify that user has entered choices + pass chosen arrays into characterbank
+  if(!capL && !lowerL && !numbersC && !specC){
+    alert('choose atleast one option')
+  }
+  if(capL){
+    characterBank = characterBank.concat(capLetters)
+  }
+  if(lowerL){
+    characterBank = characterBank.concat(lowercaseL)
+  }
+  if(numbersC){
+    characterBank = characterBank.concat(numbers)
+  }
+  if(specC){
+    characterBank = characterBank.concat(spec)
+  }
+  // for loop to pull random numbers from the newly created characterbank and push them into password1
+  for (var i=0; i< passL; i++){
+    var singleChar = characterBank[Math.floor(Math.random()* passL)]
+    password1.push(singleChar)
+  }
+//ends function and changes "password1" array to a string
+  return password1.join('')
+
+}
 //Write password to the #password input
 function writePassword() {
-  generatePassword();
-  let password = password1
+ var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
 
